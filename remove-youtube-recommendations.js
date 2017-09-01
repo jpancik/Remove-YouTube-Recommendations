67a031c1a1fc59a7bb1541013aec867cf5ad858b
Juraj Pancik <jpancik@gmail.com>
2017-09-01 15:46:43 +0200

    Updated for new YouTube design.

--- remove-youtube-recommendations.js
+++ remove-youtube-recommendations.js
@@ -8,29 +8,71 @@ function ready(fn) {
   }
 }
 
-function pluginRemoveRecommendedVideos() {
+function pluginRemoveRecommendedVideosRun() {
     //console.log("Remove YouTube Recommendations - removing garbage.");
-    Array.prototype.forEach.call(document.querySelectorAll(".video-list-item.related-list-item"), function(el, i){
-        var elSpan = el.querySelector("span.stat.view-count");
-        if(elSpan != null) {
-            //console.log(elSpan.innerText + " " + /^.*[0-9].*$/.test(elSpan.innerText));
-            if(!(/^.*[0-9].*$/.test(elSpan.innerText))) {
-               el.parentNode.removeChild(el);
-            }
-        }
-    });
+	
+	var relatedEl = document.getElementById("related");
+	
+	if (relatedEl != null) {
+		Array.prototype.forEach.call(relatedEl.getElementsByTagName("ytd-watch-next-secondary-results-renderer")[0].querySelector("#items").children, function(el, i) {
+			if(el.tagName === "YTD-COMPACT-VIDEO-RENDERER") {
+				var elSpan = el.querySelector("#metadata-line span");
+				if(elSpan != null) {
+					if(!(/^.*[0-9].*$/.test(elSpan.innerText))) {
+					   console.log("Remove YouTube Recommendations - Removing recommendation.")
+					   el.parentNode.removeChild(el);
+					}
+	        	}
+			}
+		});
+	}
+}
+
+function pluginRemoveRecommendedVideosBindToLoadMore() {
+    var target = document.getElementById("continuations");
+	if (target) {
+		var observer = new MutationObserver(function(mutations) {
+        	pluginRemoveRecommendedVideosRun();
+       	 //observer.disconnect();
+    	});
+    	var config = { childList: true };
+    	observer.observe(target, config);
+	}
+}
+
+function pluginRemoveRecommendedVideosPageLoad() {
+	setTimeout(function() {
+		pluginRemoveRecommendedVideosRun();
+	}, 1000);
+	
+	setTimeout(function() {
+		pluginRemoveRecommendedVideosRun();
+	}, 3000);
+	
+	setTimeout(function() {
+		pluginRemoveRecommendedVideosBindToLoadMore();
+	}, 2000);
+}
+
+function pluginRemoveRecommendedVideosCheckHref(checkedHref) {	
+	if(window.location.href !== checkedHref) {
+		pluginRemoveRecommendedVideosPageLoad();	
+	}
+	
+	(function (href) {
+		setTimeout(function() {
+			pluginRemoveRecommendedVideosCheckHref(href);
+		}, 500);
+	})(window.location.href);
 }
 
 ready(function() {
     //console.log("Remove YouTube Recommendations - ready!");
-    
-    pluginRemoveRecommendedVideos();
-
-    var target = document.getElementById("watch-more-related");
-    var observer = new MutationObserver(function(mutations) {
-        pluginRemoveRecommendedVideos();
-        //observer.disconnect();
-    });
-    var config = { childList: true };
-    observer.observe(target, config);
+	pluginRemoveRecommendedVideosPageLoad();
+	
+	(function (href) {
+		setTimeout(function() {
+			pluginRemoveRecommendedVideosCheckHref(href);
+		}, 500);
+	})(window.location.href);
 });
